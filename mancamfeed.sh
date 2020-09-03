722ce50d376cf8b2b8189a6f5ccc2217f196e3a7
deprov447 <ac.10edu@gmail.com>
2020-09-03 13:09:14 +0530

    fixed webcam issue

--- mancamfeed.sh
+++ mancamfeed.sh
@@ -4,6 +4,8 @@ sudo apt install -y ffmpeg
 clear 
 echo "Removing any exising virtual interface"
 sudo modprobe -r v4l2loopback
+echo "Disabling actual webcam driver"
+sudo modprobe -r uvcvideo
 echo "Creating Virtual Interface"
 sudo modprobe v4l2loopback video_nr=10 card_label="Logitech BRIO"
 for i in {1..10000}; do printf "file '%s'\n" $1 >> list.txt; done
@@ -13,6 +15,7 @@ ffmpeg -re -f concat -i list.txt -f v4l2 /dev/video10
 echo "Removing Interfaces"
 rm list.txt
 sudo modprobe -r v4l2loopback
+echo "Enabling actual webcam"
+sudo modprobe uvcvideo
 echo "Exiting ...."
 clear
-
